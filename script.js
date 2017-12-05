853e42d6fc0cd4a81d736d052845e10a7132ad4d
andrerobsahm <andrerobsahm@users.noreply.github.com>
2017-12-05 14:23:49 +0100

    Added javascript

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-const cardArray = [
-    'A','A',
-    'B','B',
-    'C','C',
-    'D','D',
-    'E','E',
-    'F','F',
-    'G','G',
-    'H','H'
-];
+'use strict';
 
-const memory_values = [];
-const memory_card_ids = [];
-const cards_flipped = 0;
+// const memory_array = [
+//     'A','A',
+//     'B','B',
+//     'C','C',
+//     'D','D',
+//     'E','E',
+//     'F','F',
+//     'G','G',
+//     'H','H'
+// ];
 
-Array.prototype.memory_card_shuffle = function() {
-    const i = this.length, j, temp;
-    while(--i > 0){
-        j = Math.floor(Math.random() * (i+1));
-        temp = this[j];
-        this[j] = this[i];
-        this[i] = temp;
+//ADD CLICK EVENT AND SEND TO THE COMPARISON ARRAY
+let cards = document.querySelectorAll('.card');
+
+cards.forEach((cardEach) => {
+    cardEach.addEventListener('click', (e) => {
+
+        let currentCard = e.target.dataset.card;
+        let compare = compArray.push(currentCard);
+
+        return compareFunction(currentCard);
+    })
+});
+
+
+
+// --- COMPARE SELECTED CARDS, AND CLEAR AFTER COMPARISON ---
+let compArray = [];
+let pairCounter = 0;
+
+let compareFunction = (dataset) => {
+    if (compArray.length == 2) {
+        if (compArray[0] == compArray[1]) {
+            console.log('Win');
+            pairCounter++; //add if match
+            console.log(pairCounter);
+
+            if (pairCounter == 8) {
+                console.log('congratz');
+            }
+        }
+
+        else {
+            console.log('loser');
+            //flip over if not a match
+        }
+
+        compArray = [];
     }
 }
 
-function newBoard() {
-    tiles_flipped = 0;
-    const output = '';
-    memory_array.memory_card_shuffle();
 
-    for (var i = 0; i < memory_array.length; i++) {
-        output += '<div id="card_'+i+'" onclick="memoryFlipCard(this,\''+memory_array[i]+'\')"></div>';
-    }
-    document.getElementById('memory_board').innerHTML = output;
+// --- SHUFFLE CARDS ---
+let memoryBoard = document.querySelector('#memory_board');
+
+for (let i = memoryBoard.children.length; i >= 0; i--) {
+    memoryBoard.appendChild(memoryBoard.children[Math.random() * i | 0]);
 }
+
+
+//--- GENERERATE NEW MEMORY BOARD ---
